2c7bcbbafd419876c0f61b8e50d3478b8017301e
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-05-21 04:24:40 -0500

    Progressive web app, added offline mode/ios compatibility

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
+
+// Register the service worker for offline capabilities
+serviceWorker.register();
