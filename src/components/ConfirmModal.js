c9bc916d3d8f355bf8accbc224297a28be317608
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-05-29 21:20:10 -0500

    Fixed modal being active in online mode

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const ConfirmModal = ({ message, onConfirm, onCancel }) => {
+const ConfirmModal = ({ show, onConfirm, onCancel, message }) => {
+  if (!show) return null;
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm w-full">
--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const RootComponent = () => {
 
   const handleReloadConfirm = () => {
     window.location.reload();
+    setShowReloadModal(false);
   };
 
   const handleReloadCancel = () => {
